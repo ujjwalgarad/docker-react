e1c43b8eb329367f09a9fcfc3ecc43edcc31db69
ujjwalgarad <22457939+ujjwalgarad@users.noreply.github.com>
2024-09-09 17:09:41 -0500

    github actions added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yo there</p>
+        <p>On Docker AWS!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
